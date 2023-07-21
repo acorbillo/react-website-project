638a4038a7e0e9336d1da2c5a719da65e68ada55
Angelo Clint Orbillo <angeloclint.dev@gmail.com>
2023-07-21 14:03:39 +0800

    fixed the chuck size warning limit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  performance: {
+    chunkSizeWarningLimit: 2097152, // 2 MB in bytes
+  },
 })
